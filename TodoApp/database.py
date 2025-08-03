9ca97fd665ac1e6113c731466762f0155c6864d6
Shalwin A S <shalwin@Shalwins-MacBook-Air.local>
2025-08-03 18:25:46 +0530

    changing the db to sqlite3

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # 1. create a loc in our fastapi app ---for SQLITE3
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:swn2001@localhost/TodoApplicationDatabase" # 1. create a loc in our fastapi app
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # 1. create a loc in our fastapi app ---for SQLITE3
+#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:swn2001@localhost/TodoApplicationDatabase" # 1. create a loc in our fastapi app
 
 #2. create engine
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #conn arg allowes us to check  ---for SQLITE3
