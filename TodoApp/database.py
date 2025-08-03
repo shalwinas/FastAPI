5f4e44ed8df4b3690f4b8e396493070203fe62c6
Shalwin A S <shalwin@Shalwins-MacBook-Air.local>
2025-08-03 18:19:20 +0530

    removing psycopg-binary and keeping only psycopg3

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # 1. create a loc in our fastapi app ---for SQLITE3
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:swn2001@localhost/TodoApplicationDatabase" # 1. create a loc in our fastapi app
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:swn2001@localhost/TodoApplicationDatabase" # 1. create a loc in our fastapi app
 
 #2. create engine
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #conn arg allowes us to check  ---for SQLITE3
