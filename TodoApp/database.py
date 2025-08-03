95695a45754cd76aa9e727339d3c4391aec0eebf
Shalwin A S <shalwin@Shalwins-MacBook-Air.local>
2025-08-03 19:06:28 +0530

    Production database change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,8 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # 1. create a loc in our fastapi app ---for SQLITE3
-#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:swn2001@localhost/TodoApplicationDatabase" # 1. create a loc in our fastapi app
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # 1. create a loc in our fastapi app ---for SQLITE3
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:gpWSHQogqJEjyvGJ@db.uxkafvxuascomhrezvhq.supabase.co:5432/postgres"# 1. create a loc in our fastapi app
+# postgresql://postgres:gpWSHQogqJEjyvGJ@db.uxkafvxuascomhrezvhq.supabase.co:5432/postgres
 
 #2. create engine
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) #conn arg allowes us to check  ---for SQLITE3
